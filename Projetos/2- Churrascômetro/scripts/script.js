0a6453be8a1b28e3a7c462ef9285eb8100b2200b
Felipe dos Santos <felipesantos.dsb@hotmail.com>
2021-02-22 18:19:42 -0300

    Edit index.html

---
+++ Projetos/2- Churrascômetro/scripts/script.js
@@ -0,0 +1,5 @@
+// Carne 400g por pessoa + de 6 horas - 650g
+// Refrigerante - 1200ml por pessoa + 6 horas - 2000ml
+// suco/agua - 1000ml por pessoa + 6 horas - 1500ml
+// crianças valem por 0,5
+
